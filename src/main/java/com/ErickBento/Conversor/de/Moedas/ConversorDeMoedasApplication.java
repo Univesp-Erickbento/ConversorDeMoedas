06c75e072738e5cf6fcc5c3896d458eb319ee051
Univesp-ErickBento <erickbento.univesp@gmail.com>
2024-10-13 23:02:25 -0300

    primeiro commit

---
+++ src/main/java/com/ErickBento/Conversor/de/Moedas/ConversorDeMoedasApplication.java
@@ -0,0 +1,13 @@
+package com.ErickBento.Conversor.de.Moedas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConversorDeMoedasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConversorDeMoedasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ErickBento/Conversor/de/Moedas/ConversorDeMoedasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ErickBento.Conversor.de.Moedas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConversorDeMoedasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
